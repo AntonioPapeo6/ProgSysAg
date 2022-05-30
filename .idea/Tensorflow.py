f44dda613460050e8e16f11d8435dd4e5e2571b2
franklin2219 <f.sasso16@studenti.uniba.it>
2022-05-30 17:05:05 +0200

    iniziata prossima fase

--- .idea/Tensorflow.py
+++ .idea/Tensorflow.py
@@ -48,7 +48,6 @@ print(f'Size of the input: {len(wav_data)}')
 Audio(wav_data, rate=sample_rate)
 
 waveform = wav_data / tf.int16.max
-print(waveform)
 # Run the model, check the output.
 scores, embeddings, spectrogram = model(waveform)
 
@@ -57,29 +56,4 @@ spectrogram_np = spectrogram.numpy()
 infered_class = class_names[scores_np.mean(axis=0).argmax()]
 print(f'The main sound is: {infered_class}')
 
-plt.figure(figsize=(10, 6))
 
-# Plot the waveform.
-plt.subplot(3, 1, 1)
-plt.plot(waveform)
-plt.xlim([0, len(waveform)])
-
-# Plot the log-mel spectrogram (returned by the model).
-plt.subplot(3, 1, 2)
-plt.imshow(spectrogram_np.T, aspect='auto', interpolation='nearest', origin='lower')
-
-# Plot and label the model output scores for the top-scoring classes.
-mean_scores = np.mean(scores, axis=0)
-top_n = 10
-top_class_indices = np.argsort(mean_scores)[::-1][:top_n]
-plt.subplot(3, 1, 3)
-plt.imshow(scores_np[:, top_class_indices].T, aspect='auto', interpolation='nearest', cmap='gray_r')
-
-# patch_padding = (PATCH_WINDOW_SECONDS / 2) / PATCH_HOP_SECONDS
-# values from the model documentation
-patch_padding = (0.025 / 2) / 0.01
-plt.xlim([-patch_padding-0.5, scores.shape[0] + patch_padding-0.5])
-# Label the top_N classes.
-yticks = range(0, top_n, 1)
-plt.yticks(yticks, [class_names[top_class_indices[x]] for x in yticks])
-_ = plt.ylim(-0.5 + np.array([top_n, 0]))
